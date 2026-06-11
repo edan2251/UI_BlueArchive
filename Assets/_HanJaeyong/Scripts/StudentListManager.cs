5c6cdc4414b53dfabc1c67c209079dfe9f48cf01
edan <molphone0328@gmail.com>
2026-06-11 18:30:57 +0900

    [Feat] 학생리스트 UI애니메이션 제작

--- Assets/_HanJaeyong/Scripts/StudentListManager.cs
+++ Assets/_HanJaeyong/Scripts/StudentListManager.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
+
+public enum FilterType
+{
+    ALL,
+    STRIKER,
+    SPECIAL
+}
+public enum SortType
+{
+    LEVEL,
+    NAME
+}
 
 public class StudentListManager : MonoBehaviour
 {
@@ -10,6 +24,24 @@ public class StudentListManager : MonoBehaviour
     [Header("[UI 연결]")]
     [SerializeField] private GameObject studentSlotPrefab;
     [SerializeField] private Transform contentTransform;
+    [SerializeField] private Image allTabImage;
+    [SerializeField] private Image strikerTabImage;
+    [SerializeField] private Image specialTabImage;
+    [SerializeField] private Sprite allActiveSprite;
+    [SerializeField] private Sprite allInactiveSprite;
+    [SerializeField] private Sprite strikerActiveSprite;
+    [SerializeField] private Sprite strikerInactiveSprite;
+    [SerializeField] private Sprite specialActiveSprite;
+    [SerializeField] private Sprite specialInactiveSprite;
+    [SerializeField] private TextMeshProUGUI sortButtonText;
+    [SerializeField] private RectTransform sortDirectionArrowRect;
+
+    [Header("매니저")]
+    [SerializeField] private UIAnimator uiAnimator;
+
+    private FilterType currentFilter = FilterType.ALL;
+    private SortType currentSortType = SortType.LEVEL;
+    private bool isDescending = true;
 
     private List<Student> runtimeStudentList = new List<Student>();
 
@@ -19,7 +51,8 @@ public class StudentListManager : MonoBehaviour
     {
         InitializeData();
         CreateUISlots();
-        SortAndRefreshUI();
+
+        SetFilter(FilterType.ALL);
     }
 
     private void InitializeData()
@@ -48,27 +81,132 @@ public class StudentListManager : MonoBehaviour
 
             if (slotUI != null)
             {
-                slotUI.Setup(student, this);
+                slotUI.Setup(student, this, uiAnimator);
 
                 slotUIDict.Add(student, slotUI);
             }
         }
     }
 
-    public void SortAndRefreshUI()
-    {
-        var sortedList = runtimeStudentList
-            .OrderByDescending(s => s.IsFavorite)
-            .ThenBy(s => s.OriginData.studentName)
-            .ToList();
+    public void OnClickTabAll() { SetFilter(FilterType.ALL, true); }
+    public void OnClickTabStriker() { SetFilter(FilterType.STRIKER, true); }
+    public void OnClickTabSpecial() { SetFilter(FilterType.SPECIAL, true); }
 
+    private void SetFilter(FilterType newFilter, bool playAnim = false)
+    {
+        if (currentFilter == newFilter) return;
+
+        currentFilter = newFilter;
+
+        UpdateTabVisuals();
+
+        SortAndRefreshUI(playAnim);
+    }
+
+    private void UpdateTabVisuals()
+    {
+        allTabImage.sprite =        (currentFilter == FilterType.ALL)       ? allActiveSprite       : allInactiveSprite;
+
+        strikerTabImage.sprite =    (currentFilter == FilterType.STRIKER)   ? strikerActiveSprite   : strikerInactiveSprite;
+
+        specialTabImage.sprite =    (currentFilter == FilterType.SPECIAL)   ? specialActiveSprite   : specialInactiveSprite;
+    }
+
+    public void OnClickSortToggle()
+    {
+        if (currentSortType == SortType.LEVEL)
+        {
+            currentSortType = SortType.NAME;
+            sortButtonText.text = "A~Z";
+        }
+        else
+        {
+            currentSortType = SortType.LEVEL;
+            sortButtonText.text = "Lv";
+        }
+
+        SortAndRefreshUI(true);
+    }
+    public void OnClickDirectionToggle()
+    {
+        isDescending = !isDescending;
+
+        //Y축 스케일 변경으로 이미지 표시
+        float targetScaleY = isDescending ? 1f : -1f;
+
+        if (sortDirectionArrowRect != null)
+        {
+            Vector3 currentScale = sortDirectionArrowRect.localScale;
+            sortDirectionArrowRect.localScale = new Vector3(currentScale.x, targetScaleY, currentScale.z);
+        }
+
+        SortAndRefreshUI(true);
+    }
+
+    public void SortAndRefreshUI(bool playAnim = false)
+    {
+        // 1순위 정렬: 즐겨찾기(내림차순 고정)
+        IOrderedEnumerable<Student> query = runtimeStudentList.OrderByDescending(s => s.IsFavorite);
+
+        // 2순위 정렬: 레벨/이름, 내림/오름차순
+        if (currentSortType == SortType.LEVEL)
+        {
+            if (isDescending)
+            {
+                // 레벨 내림차순 (높은 레벨 -> 낮은 레벨)
+                query = query.ThenByDescending(s => s.CurrentLevel);
+            }
+            else
+            {
+                // 레벨 오름차순 (낮은 레벨 -> 높은 레벨)
+                query = query.ThenBy(s => s.CurrentLevel);
+            }
+        }
+        else if (currentSortType == SortType.NAME)
+        {
+            if (isDescending)
+            {
+                // 이름 내림차순 (하 -> 가 / Z -> A)
+                query = query.ThenByDescending(s => s.OriginData.studentName);
+            }
+            else
+            {
+                // 이름 오름차순 (가 -> 하 / A -> Z)
+                query = query.ThenBy(s => s.OriginData.studentName);
+            }
+        }
+
+        var sortedList = query.ToList();
+
+        List<GameObject> activeSlotsForAnim = new List<GameObject>();
+
+        //필터타입에 따라 보여줄애들 보여주기
         foreach (var student in sortedList)
         {
             if (slotUIDict.TryGetValue(student, out StudentSlotUI slotUI))
             {
-                slotUI.transform.SetAsLastSibling();
-                slotUI.UpdateUI();
+                bool isMatch = (currentFilter == FilterType.ALL) ||
+                               (currentFilter == FilterType.STRIKER && student.OriginData.role == StudentRole.STRIKER) ||
+                               (currentFilter == FilterType.SPECIAL && student.OriginData.role == StudentRole.SPECIAL);
+
+                slotUI.gameObject.SetActive(isMatch);
+
+                if (isMatch)
+                {
+                    slotUI.transform.SetAsLastSibling();
+                    slotUI.UpdateUI();
+
+                    activeSlotsForAnim.Add(slotUI.gameObject);
+                }
             }
         }
-    }
+
+        if (playAnim && uiAnimator != null)
+        {
+            uiAnimator.PlayListPopIn(activeSlotsForAnim);
+        }
+    }//SortAndRefreshUI------End.
+
+
+
 }
--- Assets/_HanJaeyong/Scripts/StudentSlotUI.cs
+++ Assets/_HanJaeyong/Scripts/StudentSlotUI.cs
@@ -1,22 +1,28 @@
-using UnityEngine;
-using UnityEngine.UI;
+using DG.Tweening;
 using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class StudentSlotUI : MonoBehaviour
+public class StudentSlotUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
 {
     [Header("UI 연결")]
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI levelText;
     public Image favoritePinImage;
 
+    public GameObject pinClickAreaObject;
+
     private Student myStudent;
     private StudentListManager manager;
+    private UIAnimator animator;
 
-    public void Setup(Student student, StudentListManager listManager)
+    public void Setup(Student student, StudentListManager listManager, UIAnimator uiAnimator)
     {
         myStudent = student;
         manager = listManager;
+        animator = uiAnimator;
         UpdateUI();
     }
 
@@ -25,23 +31,46 @@ public class StudentSlotUI : MonoBehaviour
         nameText.text = myStudent.OriginData.studentName;
         levelText.text = $"Lv.{myStudent.CurrentLevel}";
 
-        if(favoritePinImage != null)
+        if (animator != null && favoritePinImage != null)
         {
-            if(myStudent.IsFavorite)
-            {
-                favoritePinImage.color = Color.darkOliveGreen;
-            }
-            else
-            {
-                favoritePinImage.color = Color.white;
-            }
+            animator.SetPinColorInstant(favoritePinImage, myStudent.IsFavorite);
         }
     }
 
-    // 핀버튼클릭시 호출
-    public void OnClickFavoriteToggle()
+    public void OnPointerClick(PointerEventData eventData)
     {
+        //클릭한곳이 pin영역이면 리턴
+        if (eventData.pointerPressRaycast.gameObject == pinClickAreaObject) return;
+
+        //버튼 없애고 버튼 효과 구현
+        transform.DOPunchScale(new Vector3(-0.05f, -0.05f, 0), 0.2f);
+
+        //0.15초 이따가 넘어가기
+        Invoke("OnClickDetail", 0.15f); OnClickDetail();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.pointerPressRaycast.gameObject != pinClickAreaObject) return;
+
+        //Down애니메이션
+        if (animator != null && favoritePinImage != null)
+        {
+            animator.PlayPinDown(favoritePinImage.transform);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.pointerPressRaycast.gameObject != pinClickAreaObject) return;
+
         myStudent.ToggleFavorite();
+
+        //Up애니메이션
+        if (animator != null && favoritePinImage != null)
+        {
+            animator.PlayPinUp(favoritePinImage.transform, favoritePinImage, myStudent.IsFavorite);
+        }
 
         if (manager != null)
         {
